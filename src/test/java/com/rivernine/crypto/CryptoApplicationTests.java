f271a025dc9795ab84c252a88643f892d4c77a06
rivernine <rivernine@github.com>
2021-04-15 16:57:16 +0900

    first commit

---
+++ src/test/java/com/rivernine/crypto/CryptoApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.rivernine.crypto;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class CryptoApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
